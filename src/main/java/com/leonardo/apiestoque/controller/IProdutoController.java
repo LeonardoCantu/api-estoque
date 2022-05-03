eb0ea85711b0b02ed069fb2340d1be0ad9cb5752
LeonardoCantu <leocantu.gh@gmail.com>
2022-05-03 19:07:22 -0300

    alteraçao controllers

--- src/main/java/com/leonardo/apiestoque/controller/IProdutoController.java
+++ src/main/java/com/leonardo/apiestoque/controller/IProdutoController.java
@@ -4,6 +4,7 @@ import com.leonardo.apiestoque.model.Produto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(name = "/produto")
 public interface IProdutoController {
 
+    @PostMapping
     ResponseEntity<Produto> incluir (@RequestBody Produto produto);
 
 }
